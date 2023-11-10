9985d751ef5566d69a67349c38ca18f6cd060057
meiswinkel1991 <florian.meiswinkel@outlook.de>
2023-11-10 15:38:39 +0100

    adding core functions

--- src/wallet/DiamondWallet.ts
+++ src/wallet/DiamondWallet.ts
@@ -1,82 +1,89 @@
-import { BytesLike, JsonRpcProvider, ZeroAddress, ethers } from "ethers";
-import { BundlerJsonRpcProvider, Client, UserOperationBuilder } from "userop";
 import {
-  DiamondLoupeFacet,
+  BytesLike,
+  JsonRpcProvider,
+  ZeroAddress,
+  ethers,
+  isAddress,
+} from "ethers";
+import { BundlerJsonRpcProvider, Client } from "userop";
+import {
   DiamondLoupeFacet__factory,
   SmartContractWalletFacet__factory,
+  OwnershipFacet__factory,
+  DiamondCutFacet__factory,
 } from "../typechain";
 import { DiamondWalletUserOpBuilder } from "../builder";
 import { ChainID, DiamondWalletContracts } from "../constants";
 import { UserOperationEventEvent } from "userop/dist/typechain/EntryPoint";
+import { IDiamondLoupe } from "../typechain/DiamondLoupeFacet";
+import { IDiamond } from "../typechain/DiamondCutFacet";
+
+export interface IDiomandWalletOpts {
+  walletAddress?: string;
+  salt?: bigint;
+}
 
 export class DiamondWallet {
   private walletAddress: string;
   private signer: ethers.Signer;
   private publicProvider: JsonRpcProvider;
-  private userOPBuilder: DiamondWalletUserOpBuilder | null;
-  private client: Client | null;
+  private userOPBuilder: DiamondWalletUserOpBuilder;
+  private client: Client;
   private functions: Array<BytesLike>;
-  private facets: Array<string>;
-  private diamondLoupe: DiamondLoupeFacet;
+  private isDeployed: boolean;
 
-  private constructor(signer: ethers.Signer, publicRpcUrl: string) {
-    this.publicProvider = new JsonRpcProvider(publicRpcUrl);
+  private constructor(
+    signer: ethers.Signer,
+    publicProvider: JsonRpcProvider,
+    client: Client,
+    builder: DiamondWalletUserOpBuilder
+  ) {
+    this.publicProvider = publicProvider;
 
     this.signer = signer;
     this.walletAddress = ZeroAddress;
-    this.userOPBuilder = null;
-    this.client = null;
+    this.userOPBuilder = builder;
+    this.client = client;
     this.functions = [];
-    this.facets = [];
-    this.diamondLoupe = DiamondLoupeFacet__factory.connect(ZeroAddress);
+    this.isDeployed = false;
   }
 
   public static async init(
     signer: ethers.Signer,
     bundlerRpcUrl: string,
     rpcUrl: string,
-    salt?: bigint
+    opts?: IDiomandWalletOpts
   ) {
-    const instance = new DiamondWallet(signer, rpcUrl);
+    const publicProvider = new JsonRpcProvider(rpcUrl);
 
-    const bundler = new BundlerJsonRpcProvider(bundlerRpcUrl);
-
-    const chainId = (await instance.publicProvider.getNetwork()).chainId;
+    const chainId = (await publicProvider.getNetwork()).chainId;
 
     const contracts = DiamondWalletContracts[Number(chainId)];
-    console.log(chainId);
-    console.log(contracts);
 
-    let entryPoint;
+    const entryPoint = contracts.EntryPoint;
+    const bundler = new BundlerJsonRpcProvider(bundlerRpcUrl);
 
-    entryPoint = contracts.EntryPoint;
-
-    instance.userOPBuilder = await DiamondWalletUserOpBuilder.init(
+    const builder = await DiamondWalletUserOpBuilder.init(
       signer,
       bundler,
       contracts.Factory,
-      { entryPoint: entryPoint, salt: salt }
+      { entryPoint: entryPoint, salt: opts?.salt }
     );
+
+    const client = await Client.init(bundlerRpcUrl);
+
+    const instance = new DiamondWallet(signer, publicProvider, client, builder);
 
     instance.walletAddress = instance.userOPBuilder.getSender();
-    instance.diamondLoupe = DiamondLoupeFacet__factory.connect(
-      instance.walletAddress,
-      instance.publicProvider
-    );
 
-    instance.client = await Client.init(bundlerRpcUrl);
-
-    // const facets = await instance.diamondLoupe.facets();
-    // console.log(facets);
-    // facets.forEach((facet) => {
-    //   instance.facets.push(facet.facetAddress);
-    //   const selectors = facet.functionSelectors;
-    //   selectors.map((selector) => {
-    //     instance.functions.push(selector);
-    //   });
-    // });
+    instance.isDeployed = await instance.checkDeployment();
 
     return instance;
+  }
+
+  private async checkDeployment(): Promise<boolean> {
+    const byteCode = await this.publicProvider.getCode(this.walletAddress);
+    return byteCode.length != 0;
   }
 
   /* ====== Getter Functions ====== */
@@ -85,25 +92,45 @@ export class DiamondWallet {
   }
 
   getUserOpBuilder(): DiamondWalletUserOpBuilder | null {
-    if (!this.userOPBuilder) return null;
     return this.userOPBuilder;
   }
 
   getClient(): Client | null {
-    if (!this.client) return null;
     return this.client;
-  }
-
-  getFacetAddresses(): Array<string> {
-    return this.facets;
-  }
-
-  getFunctions(): Array<BytesLike> {
-    return this.functions;
   }
 
   getActiveSigner(): ethers.Signer {
     return this.signer;
+  }
+
+  getDeploymentStatus(): boolean {
+    return this.isDeployed;
+  }
+
+  /* ======= Core Facet Getter Functions ====== */
+
+  async getFacetAddresses(): Promise<Array<string>> {
+    if (!this.isDeployed) throw Error("Diamond Wallet not deployed!!");
+    return await DiamondLoupeFacet__factory.connect(
+      this.walletAddress,
+      this.publicProvider
+    ).facetAddresses();
+  }
+
+  async getFacets(): Promise<Array<IDiamondLoupe.FacetStructOutput>> {
+    if (!this.isDeployed) throw Error("Diamond Wallet not deployed!!");
+    return await DiamondLoupeFacet__factory.connect(
+      this.walletAddress,
+      this.publicProvider
+    ).facets();
+  }
+
+  async getWalletOwner(): Promise<string> {
+    if (!this.isDeployed) throw Error("Diamond Wallet not deployed!!");
+    return await OwnershipFacet__factory.connect(
+      this.walletAddress,
+      this.publicProvider
+    ).owner();
   }
 
   /* ====== Wallet Interactions ======*/
@@ -111,7 +138,6 @@ export class DiamondWallet {
   private async sendUserOp(
     builder: DiamondWalletUserOpBuilder
   ): Promise<UserOperationEventEvent | null> {
-    if (!this.client) throw Error("No Client initialized!");
     try {
       console.log(builder.getOp());
       const res = await this.client.sendUserOperation(builder);
@@ -127,17 +153,79 @@ export class DiamondWallet {
   /* ====== Core Facet Interactions ====== */
 
   async execute(
-    to: string,
+    dest: string,
     value: bigint,
     data: BytesLike
   ): Promise<UserOperationEventEvent | null> {
-    if (!this.userOPBuilder)
-      throw Error("UserOpertaionBuilder not initialized!");
+    if (!isAddress(dest))
+      throw Error(`Destintation ${dest} is not a valid evm address`);
     const intrf = SmartContractWalletFacet__factory.connect(this.walletAddress);
     const functionCallData = intrf.interface.encodeFunctionData("execute", [
-      to,
+      dest,
       value,
       data,
+    ]);
+
+    return await this.sendUserOp(
+      this.userOPBuilder.setCallData(functionCallData)
+    );
+  }
+
+  async executeBatch(
+    dests: Array<string>,
+    values: Array<bigint>,
+    datas: Array<BytesLike>
+  ): Promise<UserOperationEventEvent | null> {
+    dests.forEach((dest) => {
+      if (!isAddress(dest))
+        throw Error(`Destintation ${dest} is not a valid evm address`);
+    });
+    const intrf = SmartContractWalletFacet__factory.connect(this.walletAddress);
+    const functionCallData = intrf.interface.encodeFunctionData(
+      "executeBatch",
+      [dests, values, datas]
+    );
+    return await this.sendUserOp(
+      this.userOPBuilder.setCallData(functionCallData)
+    );
+  }
+
+  async diamondCut(
+    facetCuts: Array<IDiamond.FacetCutStruct>,
+    init: string,
+    initData: BytesLike
+  ): Promise<UserOperationEventEvent | null> {
+    if (!isAddress(init))
+      throw Error(`Init ${init} is not a valid evm address`);
+
+    const intrf = DiamondCutFacet__factory.connect(
+      this.walletAddress
+    ).interface;
+
+    const functionCallData = intrf.encodeFunctionData("diamondCut", [
+      facetCuts,
+      init,
+      initData,
+    ]);
+
+    return await this.sendUserOp(
+      this.userOPBuilder.setCallData(functionCallData)
+    );
+  }
+
+  async transferOwnership(
+    newOwner: string
+  ): Promise<UserOperationEventEvent | null> {
+    if (!isAddress(newOwner))
+      throw Error(`New owner ${newOwner} is not a valid evm address`);
+
+    const intrf = OwnershipFacet__factory.connect(
+      this.walletAddress,
+      this.publicProvider
+    ).interface;
+
+    const functionCallData = intrf.encodeFunctionData("transferOwnership", [
+      newOwner,
     ]);
 
     return await this.sendUserOp(
