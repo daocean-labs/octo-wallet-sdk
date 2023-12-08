341ccf2434bea8939823d8f5a146a09fb71b3e2b
meiswinkel1991 <florian.meiswinkel@outlook.de>
2023-12-08 10:16:57 +0100

    new factory

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -9,7 +9,7 @@ export const ChainID = {
 export const OctoDefiContracts: ContractLib = {
   1337: {
     EntryPoint: "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
-    Factory: "0xD52d8bb7Ec439DAA364a3b6517005EeEc9324B06",
+    Factory: "0x9Bcb695Ea0BBeC9f35417A6B36Bd8C936dafdD20",
   },
 };
 
